8395229927dacd3ee90aa75527c98445518d3c73
AbhiDroid <abhinabroy2001@gmail.com>
2022-09-03 20:12:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const getQRbtn =  document.getElementById('get-qr');
+const QRDisplay =  document.getElementById('qr-display');
+const name = document.getElementById('name');
+const msg = document.getElementById('msg');
+const day = document.getElementById('day');
+const month = document.getElementById('month');
+const year = document.getElementById('year');
+const hour = document.getElementById('hour');
+const min = document.getElementById('min');
+
+const getQR = () => {
+    console.log("hello")
+    const time = {};
+    time.day = day.value;
+    time.month = month.value-1;
+    time.year = year.value;
+    time.hour = hour.value;
+    time.min = min.value;
+    const payload = {};
+    payload.name = name.value;
+    payload.msg = msg.value;
+    axios.post("http://127.0.0.1:3000/whatsapp-api", {payload, time}).then(response => {
+        console.log(response.data.qr);
+        var qrcode = new QRCode(QRDisplay, {
+            text: response.data.qr,
+            width: 128,
+            height: 128,
+            colorDark : "#000000",
+            colorLight : "#ffffff",
+            correctLevel: QRCode.CorrectLevel.L
+        });
+    }).catch(err => {
+        console.log(err);
+    })
+
+
+}
+
+
+
+getQRbtn.addEventListener('click', getQR);
